7b80bfef6230fb37c04d746646fedaa0c59150fb
imstrive <longtaosu@sina.com>
2019-04-01 21:07:23 +0800

    修改端口

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ docker stop jenkins2docker;
 docker rm jenkins2docker;
 docker build -t jenkins2docker:$image_version .;
 docker images;
-docker run -p 10000:80 -d --name jenkins2docker jenkins2docker:$image_version;
+docker run -p 10001:80 -d --name jenkins2docker jenkins2docker:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 docker logs jenkins2docker;
 #删除build过程中产生的镜像    #docker image prune -a -f
