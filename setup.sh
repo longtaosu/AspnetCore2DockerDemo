cd9228719f08da058565b65b82946a1252b833b0
imstrive <longtaosu@sina.com>
2019-04-01 20:27:06 +0800

    将镜像名称换为小写

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 image_version=`date +%Y%m%d%H%M`;
-project_name=`Jenkins2Docker`;
 echo $image_version;
 # cd Jenkins2Docker
 git pull --rebase origin master;
-docker stop Jenkins2Docker;
-docker rm Jenkins2Docker;
-docker build -t Jenkins2Docker:$image_version .;
+docker stop jenkins2docker;
+docker rm jenkins2docker;
+docker build -t jenkins2docker:$image_version .;
 docker images;
-docker run -p 10000:80 -d --name Jenkins2Docker Jenkins2Docker:$image_version;
+docker run -p 10000:80 -d --name jenkins2docker jenkins2docker:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs Jenkins2Docker;
+docker logs jenkins2docker;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
